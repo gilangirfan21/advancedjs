d75cacc546246b760140c193cf06ac3e9a180dd8
gilangirfan21 <gilangxirfan21@gmail.com>
2021-02-25 05:52:49 +0700

    Pembelajaran object javascript 1,2,3

--- object/script.js
+++ object/script.js
@@ -47,54 +47,25 @@
 
 
 // 3. Constraction Function (keyword new)
-// function Mahasiswa(nama, energi){ //used capital for function
-//         this.nama = nama;
-//         this.energi = energi;
+function Mahasiswa(nama, energi){ //used capital for function
+        this.nama = nama;
+        this.energi = energi;
     
-//         this.makan = function(porsi){
-//             this.energi += porsi;
-//             console.log(`Halo ${this.nama}, selamat makan!`);
-//         }
+        this.makan = function(porsi){
+            this.energi += porsi;
+            console.log(`Halo ${this.nama}, selamat makan!`);
+        }
     
-//         this.main = function(jam){
-//             this.energi -= jam;
-//             console.log(`Halo ${this.nama}, selamat bermain!`);
-//         }
+        this.main = function(jam){
+            this.energi -= jam;
+            console.log(`Halo ${this.nama}, selamat bermain!`);
+        }
     
-//     }
+    }
 
 
-// let gilang = new Mahasiswa("Gilang",10);
-// let irfan = new Mahasiswa("Irfan",20)
+let gilang = new Mahasiswa("Gilang",10);
+let irfan = new Mahasiswa("Irfan",20)
 
 
 // 4. Object.create
-//Lebih efektif dan penulisan lebih ringkas dibanding 1,2,3
-const methodMahasiswa = {
-    makan: function(porsi){
-        this.energi += porsi;
-        console.log(`Halo ${this.nama}, selamat makan!`);
-    },
-
-    main: function(jam){
-        this.energi -= jam;
-        console.log(`Halo ${this.nama}, selamat bermain!`);
-    },
-
-    tidur: function(jam) {
-        this.energi += jam * 2;
-        console.log(`Halo ${this.nama}, selamat tidur!`);
-    }
-}
-
-function Mahasiswa(nama, energi){ //used capital for function
-        let mahasiswa = Object.create(methodMahasiswa);
-        mahasiswa.nama = nama;
-        mahasiswa.energi = energi;
-
-        return mahasiswa;
-
-}
-
-    let gilang = Mahasiswa("Gilang",10);
-    let irfan = Mahasiswa("Irfan",20); 
