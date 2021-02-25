f2941ecde8bb85d1ee6624d867453e0886363cb9
gilangirfan21 <gilangxirfan21@gmail.com>
2021-02-26 00:37:07 +0700

    Belajar Object prototype javascript

--- object/script.js
+++ object/script.js
@@ -70,31 +70,60 @@
 
 // 4. Object.create
 //Lebih efektif dan penulisan lebih ringkas dibanding 1,2,3
-const methodMahasiswa = {
-    makan: function(porsi){
-        this.energi += porsi;
-        console.log(`Halo ${this.nama}, selamat makan!`);
-    },
 
-    main: function(jam){
-        this.energi -= jam;
-        console.log(`Halo ${this.nama}, selamat bermain!`);
-    },
+//Model function declaration
+// const methodMahasiswa = {
+//     makan: function(porsi){
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, selamat makan!`);
+//     },
 
-    tidur: function(jam) {
-        this.energi += jam * 2;
-        console.log(`Halo ${this.nama}, selamat tidur!`);
-    }
-}
+//     main: function(jam){
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain!`);
+//     },
 
+//     tidur: function(jam) {
+//         this.energi += jam * 2;
+//         console.log(`Halo ${this.nama}, selamat tidur!`);
+//     }
+// }
+
+// function Mahasiswa(nama, energi){ //used capital for function
+//         let mahasiswa = Object.create(methodMahasiswa);
+//         mahasiswa.nama = nama;
+//         mahasiswa.energi = energi;
+
+//         return mahasiswa;
+
+// }
+
+//     let gilang = Mahasiswa("Gilang",10);
+//     let irfan = Mahasiswa("Irfan",20); 
+
+
+//Model Constraction Function
 function Mahasiswa(nama, energi){ //used capital for function
-        let mahasiswa = Object.create(methodMahasiswa);
-        mahasiswa.nama = nama;
-        mahasiswa.energi = energi;
-
-        return mahasiswa;
-
+        //let this = Object.create(Mahasiswa.prototye); ->secara otomatis membuat variabel this, parent object Mahasiswa.prototye
+        this.nama = nama;
+        this.energi = energi;
 }
 
-    let gilang = Mahasiswa("Gilang",10);
-    let irfan = Mahasiswa("Irfan",20); 
+Mahasiswa.prototype.makan = function (prosi) {
+    this.energi += prosi;
+    return console.log(`Halo ${this.nama}, selamat makan!`);
+}
+
+Mahasiswa.prototype.main = function (jam) {
+    this.energi -= jam;
+    return console.log(`Helo ${this.nama}, selamat bermain!`);
+}
+
+Mahasiswa.prototype.tidur = function (jam) {
+    this.energi += jam * 2;
+    return console.log(`Helo ${this.nama}, selamat tidur!`);
+}
+
+
+    let gilang = new Mahasiswa("Gilang",10);
+    let irfan = new Mahasiswa("Irfan",20); 
